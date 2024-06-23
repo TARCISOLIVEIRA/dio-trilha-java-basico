200308c4f53e9718cc83cb9d231ffa0f196a7d23
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2024-06-23 15:59:10 -0300

    Exercicio Variavel aula 3

--- tipo-variaveis/src/App.java
+++ tipo-variaveis/src/App.java
@@ -2,5 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello,.......  World!");
         System.out.println("bem como ");
+
+        short numeroCurto  = 10;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+         
+
+
+
+
     }
 }
