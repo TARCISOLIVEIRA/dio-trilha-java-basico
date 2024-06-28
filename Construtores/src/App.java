36a0cb2c7d5f30e4dd5b2715e33ec5bc88fd3af1
Tarciso dos Santos de Oliveira <84403833+TARCISOLIVEIRA@users.noreply.github.com>
2024-06-28 09:44:20 -0300

    Atualizar

---
+++ Construtores/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        Aluno aluno = new Aluno();
+        aluno.nome = "Tarciso";
+        aluno.idade = 25;
+
+        System.out.println("aluno.....: " + aluno.nome); 
+
+
+    }
+}
